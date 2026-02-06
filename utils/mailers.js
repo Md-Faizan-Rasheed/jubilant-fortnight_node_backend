d7e07840f14edbc2807192f2081005a754bff798
Md-Faizan-Rasheed <s1391792@gmail.com>
2026-02-06 10:43:31 +0530

    add sendgrid

--- utils/mailers.js
+++ utils/mailers.js
@@ -1,16 +1,32 @@
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.EMAIL_USER, // your email
+//     pass: process.env.EMAIL_PASS, // app password
+//   },
+// });
+
+
+// module.exports = {
+//   transporter
+// }
+
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",   // 👈 IMPORTANT
   auth: {
-    user: process.env.EMAIL_USER, // your email
-    pass: process.env.EMAIL_PASS, // app password
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, // App Password
   },
+  connectionTimeout: 10 * 1000, // 10 seconds
+  greetingTimeout: 10 * 1000,
+  socketTimeout: 10 * 1000,
 });
 
-
-module.exports = {
-  transporter
-}
+module.exports = transporter;
